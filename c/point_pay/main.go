fbe013bb0496fe3a63503d2b7130cd6fb919e67d
yokkoi21 <takuya.y.211540@gmail.com>
2020-02-02 01:21:52 +0900

    add point pay

--- c/point_pay/main.go
+++ c/point_pay/main.go
@@ -7,28 +7,29 @@ import "bufio"
 
 func main(){
 	var sc = bufio.NewScanner(os.Stdin)
-	var input [1001]string
+	var input [101]string
 	var split_input []string
 	var i int
-	var day,price int
-	var point_sum int
+	var balance,point int
+	var fee int
 	for sc.Scan(){
 		input[i] = sc.Text()
 		i++
 	}
-	n,_ := strconv.Atoi(input[0])
-	for i = 1;i < n+1;i++{
-		split_input = strings.Split(input[i]," ")
-		day,_ = strconv.Atoi(split_input[0])
-		price,_ = strconv.Atoi(split_input[1])
-		if day == 3 || day == 13 || day ==23 || day == 30 || day == 31{
-			point_sum += (price * 3) / 100
-		}else if day == 5 || day == 15 || day ==25 {
-			point_sum += (price * 5) / 100
-		}else {
-			point_sum += (price * 1) / 100
+	split_input = strings.Split(input[0]," ")
+	n,_ := strconv.Atoi(split_input[0])
+	m,_ := strconv.Atoi(split_input[1])
+	balance = n
+	for i = 1;i < m+1;i++{
+		fee,_ = strconv.Atoi(input[i])
+		if fee <= point{
+			point -= fee
+		}else{
+			balance -= fee
+			point += (fee / 10)
 		}
+		fmt.Printf("%d %d\n",balance,point)
 	}
-	fmt.Println(point_sum)
+	
 	
 }

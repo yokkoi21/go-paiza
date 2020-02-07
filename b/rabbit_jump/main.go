b5241747b2faa0729f62e7ce6650940d08beed33
yokkoi21 <takuya.y.211540@gmail.com>
2020-02-07 20:32:29 +0900

    complete rabbit_jump

--- b/rabbit_jump/main.go
+++ b/rabbit_jump/main.go
@@ -10,10 +10,9 @@ func main(){
 	var input [101]string
 	var split_input []string
 	var n,m,k int
-	var i,j int
-	var r_position_tmp int
-	var r_position [51]int
-	var ans_tmp [51]int
+	var i,j,l int
+	var r_position [501]int
+	var r_position_tmp [501]int
 
 	i = 0
 	for sc.Scan(){
@@ -25,23 +24,64 @@ func main(){
 	m,_ = strconv.Atoi(split_input[1])
 	k,_ = strconv.Atoi(split_input[2])
 	
-	 
-	for i = 1;i < h+1;i++{
-		r_postion[i],_ = strconv.Atoi(input[i])
+	// for i = 1;i < 501;i++{
+	// 	r_position_tmp[r_position[i]] = 0
+	// }
+	for i = 1;i < m+1;i++{
+		r_position[i],_ = strconv.Atoi(input[i])
+		r_position_tmp[r_position[i]] = 1
 	}
+	// for i = 1;i < m+1;i++{
+	// 	if r_position[i] != 0{
+	// 		fmt.Printf("%d",r_position[i])
+	// 	}	
+	// }
+	// fmt.Printf("\n")
+
+	// for i = 1;i < 10;i++{
+	// 	fmt.Printf("%d",r_position_tmp[i])	
+	// }
 	
 	for i = 0;i < k;i++{
 		for j = 1;j < m+1;j++{
-			if r_position[]
+			for l = 1;l < n;l++{
+				if r_position[j]+l <= n{
+					if r_position_tmp[r_position[j]+l] == 0{
+						r_position[j] = r_position[j]+l
+						r_position_tmp[r_position[j]] = 1
+						r_position_tmp[r_position[j]-l] = 0
+						break
+					}else{
+						continue
+					}
+				}else{
+					if r_position_tmp[r_position[j]+l-n] == 0{
+						r_position[j] = r_position[j]+l-n
+						r_position_tmp[r_position[j]] = 1
+						r_position_tmp[r_position[j]-l+n] = 0
+						break
+					}else{
+						continue
+					}
+				}
+			}
 		}
 	}
-	
-	for i = 0;i < h;i++{
-		for j = 0;j < w;j++{
-			ans_tmp[i] += graph[i][j]
-		}
-		fmt.Println(ans_tmp[i])
-		ans_tmp[i] = ""
+	//fmt.Printf("\n")
+	for i = 0;i < n;i++{
+		if r_position[i] != 0{
+			fmt.Println(r_position[i])
+		}	
 	}
+	// for i = 1;i < m+1;i++{
+	// 	if r_position[i] != 0{
+	// 		fmt.Printf("%d",r_position[i])
+	// 	}	
+	// }
+	// fmt.Printf("\n")
+
+	// for i = 1;i < 10;i++{
+	// 	fmt.Printf("%d",r_position_tmp[i])	
+	// }
 	
 }

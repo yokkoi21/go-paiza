9944f408e559bae7ba459c8302bb18ed583526b0
yokkoi21 <takuya.y.211540@gmail.com>
2020-03-23 00:55:54 +0900

    a/beem_reflection & b/3d_printer

--- b/3d_printer/main.go
+++ b/3d_printer/main.go
@@ -12,6 +12,7 @@ func main(){
 	var i,j,k int
 	var x,y,z int
 	var count int
+	var sharp_check int
 	
 	
 
@@ -35,22 +36,48 @@ func main(){
 	ans_graph := make([][]string, y)
 	for i:=0; i<x; i++{
 		ans_graph[i] = make([]string, z)
-		
 	}	
 	
-	count = 0
+	count = 1
 	for k = 0;k < z;k++{
 		for j = 0;j < y;j++{
-			if input[count] == "--"{
-				count++
-			}else{
+			if input[count] != "--"{
 				split_input = strings.Split(input[count],"")
 				for i = 0;i < x;i++{
 					graph[i][j][k] = split_input[i]
+					//fmt.Printf(graph[i][j][k])
 				}
+				count++
+			}else{
+				count++
+				j--
 			}
-			count++
+			//fmt.Printf("\n")
 		}
+	}
+
+	x--
+	for k = 0;k < z;k++{
+		for j = 0;j < y;j++{
+			for i = 0;i < x;i++{
+				if graph[i][j][k] == "#"{
+					sharp_check = 1
+				}
+				//fmt.Printf(graph[i][j][k])
+			}
+			if sharp_check == 1{
+				ans_graph[j][k] = "#"
+			}
+			sharp_check = 0
+		}
+	}
+
+	for k = 0;k < z;k++{
+		for j = 0;j < y;j++{
+			fmt.Printf(ans_graph[k][j])
+			
+		}
+		fmt.Printf("\n")
 	}
 	
 	
